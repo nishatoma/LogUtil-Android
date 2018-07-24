e65add51234a578db8d6185be378a55288942877
nishatoma <nishatoma@gmail.com>
2018-07-23 20:42:55 -0400

    Create project in Studio. Create LogDebug Class.

---
+++ logutil/src/main/java/com/util/league95/logutil/LogDebug.java
@@ -0,0 +1,12 @@
+package com.util.league95.logutil;
+
+import android.util.Log;
+
+public class LogDebug {
+
+    private static final String TAG = "Example TAg";
+
+    public static void d(String message) {
+        Log.d(TAG, message);
+    }
+}
